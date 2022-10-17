3fa8adbd5e53442e0313e16b91d1b0f76475d3ce
The Buildbot <buildbot@angular2buch.de>
2022-10-17 14:52:55 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/c6a12f705591f970e6f2e9b74af5f9ef7ce5da12

--- src/app/books/book-list-item/book-list-item.component.spec.ts
+++ src/app/books/book-list-item/book-list-item.component.spec.ts
@@ -1,25 +1,48 @@
+import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { BookListItemComponent } from './book-list-item.component';
+
+@Component({ })
+class TestDetailsComponent { }
 
 describe('BookListItemComponent', () => {
   let component: BookListItemComponent;
   let fixture: ComponentFixture<BookListItemComponent>;
+  let location: Location;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BookListItemComponent ]
+      declarations: [BookListItemComponent],
+      imports: [
+        RouterTestingModule.withRoutes([
+          { path: ':isbn', component: TestDetailsComponent }
+        ])
+      ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    location = TestBed.inject(Location);
     fixture = TestBed.createComponent(BookListItemComponent);
     component = fixture.componentInstance;
+    // Buch zum Anzeigen
+    component.book = {
+      isbn: '111',
+      title: 'Book 1',
+      authors: [],
+      thumbnailUrl: 'https://ng-buch.de/kochen.jpg'
+    };
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should navigate to details page by ISBN', async () => {
+
+    fixture.nativeElement.querySelector('img').click();
+    await fixture.whenStable();
+    expect(location.path()).toEqual('/111');
   });
 });
